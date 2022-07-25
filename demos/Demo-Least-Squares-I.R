d78209dbed41780397b29594ac3713b990cadd7c
chadtopaz <chad.topaz@gmail.com>
2022-07-25 12:43:22 -0400

    Edits to Least Squares I

--- demos/Demo-Least-Squares-I.R
+++ demos/Demo-Least-Squares-I.R
@@ -6,11 +6,7 @@ library(pracma)
 a <- 3:6
 s <- c(105,117,141,152)
 plot(a, s, pch = 19)
-vandermonde <- function(x) {
-  A <- outer(x, (length(x)):1 - 1, "^")
-  return(A)
-}
-A <- vandermonde(a)[,c(3,4)]
+A <- vander(a)[, c(3,4)]
 
 # Least squares with one basis vector
 # Find best solution x (scalar) to ax = b
@@ -22,13 +18,13 @@ bhat <- a %*% x
 r <- b - bhat
 print(x)
 print(bhat)
-dot <- function(v1,v2){sum(v1*v2)}
+dot <- function(v1, v2){sum(v1*v2)}
 dot(a,r)
 
 # Least squares with two basis vectors
 # Advertising example from lecture notes
 a <- 3:6
-A <- vandermonde(a)[,c(3,4)]
+A <- vander(a)[, c(3,4)]
 b <- c(105,117,141,152)
 pseudoinv <- solve(t(A) %*% A) %*% t(A)
 x <- pseudoinv %*% b
@@ -38,13 +34,13 @@ print(x)
 print(bhat)
 dot(r,A[,1])
 dot(r,A[,2])
-plot(a1,b,xlab="advertising",ylab="sales")
-xx <- seq(from=0,to=6,length=200)
-lines(xx,horner(as.numeric(x),xx)$y)
+plot(a,b,xlab="advertising",ylab="sales")
+xx <- seq(from = 0, to = 6, length = 200)
+lines(xx, horner(x, xx)$y)
 
 # Best-fit parabola
 x <- c(0,1,2,3)
-A <- vandermonde(x)[,2:4]
+A <- vander(x)[, 2:4]
 b <- c(6,5,2,2)
 pseudoinv <- solve(t(A) %*% A) %*% t(A)
 c <- pseudoinv %*% b
@@ -52,14 +48,15 @@ bhat <- A %*% c
 r <- b - bhat
 print(c)
 plot(x,b)
-xx <- seq(from=0,to=3,length=200)
-lines(xx,horner(c,xx)$y)
+xx <- seq(from = 0, to = 3, length = 200)
+lines(xx, horner(c, xx)$y)
 print(r)
 
 # Data compression
-x <- seq(from=0,to=1,length=100)
+x <- seq(from = 0, to = 1, length = 100)
 y <- 3*x + 0.4*(2*runif(100))
 plot(x,y)
-A <- vandermonde(x)[,(length(x)-1):length(x)]
-c <- qr.solve(A,y)
-lines(x, horner(c,x)$y, col = "red")
+A <- vander(x)[, (length(x)-1):length(x)]
+pseudoinv <- solve(t(A) %*% A) %*% t(A)
+c <- pseudoinv %*% y
+lines(x, horner(c, x)$y, col = "red")
